e68ae42e92c89c1f2d919dda07f34e4be55e34c8
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2022-05-12 09:48:28 +0200

    creation de pastille

---
+++ pacman-application/pastille.cpp
@@ -0,0 +1,18 @@
+#include "pastille.h"
+#include "drawableelem.h"
+#include "ObserverCollision.h"
+#include <iostream>
+
+Pastille::Pastille()
+{
+
+}
+bool test_collision(int x,int y)
+{
+
+};
+void collision(NotifierCollision notifeur)
+{
+
+};
+
---
+++ pacman-application/pastille.h
@@ -0,0 +1,13 @@
+#ifndef PASTILLE_H
+#define PASTILLE_H
+#include "observercollision.h"
+#include "drawableelem.h"
+class Pastille : public ObserverCollision, public DrawableElem
+{
+public:
+    Pastille();
+    bool test_collision(int x,int y);
+    void collision(NotifierCollision notifeur);
+};
+
+#endif // PASTILLE_H
